c28815cd3039410434c48ed8962d20ac9dc0c0c3
ryanjsims <31775294+ryanjsims@users.noreply.github.com>
2017-09-09 22:19:29 -0700

    Added comments

--- proj02/powers.c
+++ proj02/powers.c
@@ -1,16 +1,29 @@
+/* powers.c
+ * Programmer: Ryan Sims
+ * Date: 9/9/17
+ *
+ * Powers is a short program that iteratively prints out powers of a base number.
+ * It accepts input from stdin in the form of 3 ints: number, count, and modulo.
+ * It then prints out the powers of number from 0 to count - 1, and if any of them are
+ * multiples of modulo, it notes that event. After all powers are output, it
+ * prints the total number of multiples to the screen.
+ */
+
 #include <stdio.h>
 
 int main(void){
 	int num, cnt, mod, product = 1, total = 0;
-	if(scanf("%d %d %d", &num, &cnt, &mod) < 3){
+	if(scanf("%d %d %d", &num, &cnt, &mod) < 3){ //Check that all parameters were found.
 		fprintf(stderr, "Did not read all 3 parameters.\n");
 		return 1;
-	} else if(num < 2 || mod < 2){
-		fprintf(stderr, "num and modulo must both be >= 2");
+	} else if(num < 2 || mod < 2){ //Ensure we get interesting results with num and mod > 1
+		fprintf(stderr, "num and modulo must both be > 1");
 		return 1;
 	}
+	//Print cnt - 1 powers of num by decrementing cnt to 0
 	while(cnt > 0){
 		printf("%d", product);
+		//Check for multiples of mod using the modulo operator
 		if(product % mod == 0){
 			printf(" is a multiple of %d", mod);
 			total++;
--- proj02/squares.c
+++ proj02/squares.c
@@ -1,48 +1,78 @@
+/* squares.c
+ * Programmer: Ryan Sims
+ * Date: 9/9/17
+ * 
+ * Squares is a short program that prints squares of letters, slashes, and stars
+ * to stdout. Squares takes one int size followed by any number of chars as input, and
+ * will output squares of each char of dimension size*size.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+//drawSquare draws a single square to stdout using size and fill.
 int drawSquare(int size, char fill);
 
+//Main function to control input and exit conditions
 int main(void){
+	//Allocate pointers because I don't want to use & in scanf
 	int *size = (int*)malloc(sizeof(int));
 	char *fill = (char*)malloc(sizeof(char));
+	
+	//Flag to control output of newline.
+	//Prevents extra newline from being printed at beginning of output
 	int result = -1;
+	
+	//Check that size is found
 	if(!scanf("%d", size)){
 		fprintf(stderr, "Size not found in input.\n");
 		return 1;
-	} else if(*size <= 0){
+	} else if(*size <= 0){ //Check that size is in correct range
 		fprintf(stderr, "Size must be positive and nonzero.\n");
 		return 1;
 	}
+	//Scan characters until no more characters can be scanned
+	//and use drawSquare to draw a square with each scanned char
 	while(scanf("%c", fill) > 0){
 		if(result != -1)
 			printf("\n");
-		result = drawSquare(*size, *fill);
+		result = drawSquare(*size, *fill); //Derefence pointers to values
 	}
+	//Free allocated pointers to avoid (insignificant) memory leak
 	free(size);
 	free(fill);
 	return 0;
 }
 
+//drawSquare draws a single square to stdout using size and fill.
 int drawSquare(int size, char fill){
 	int i, j;
+	//Check that fill is printable and print error and hex code if not
 	if(!isprint(fill)){
 		printf("The input character is not a printable character.  Hex=0x%02x\n", fill);
 		return 1;
 	}
+	//Draw square of size * size
 	for(i = 0; i < size; i++){
 		for(j = 0; j < size; j++){
+			//Check if current location is center of odd size
 			if(i == (size - 1) / 2 && i == j && size % 2 != 0)
 				printf("*");
+			//Check if loc is on TL to BR diagonal
 			else if(i == j)
 				printf("\\");
+			//Check if loc is on TR to BL diagonal
 			else if(size - 1 - i == j)
 				printf("/");
+			//Check if loc is on top or bottom edge
 			else if(i == 0 || i == size - 1)
 				printf("-");
+			//Check if loc is on left or right edge
 			else if(j == 0 || j == size - 1)
 				printf("|");
+			//After all special cases checked, print fill
 			else
 				printf("%c", fill);
 		}
