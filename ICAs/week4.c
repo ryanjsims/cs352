c0e8b368c0b37f3e4e9ca67a178c518be9d3b24b
Ryan <ryanjsims@email.arizona.edu>
2017-09-15 13:24:42 -0700

    ICA

--- ICAs/week4.c
+++ ICAs/week4.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-void main(){
-    int x = 0;
-    int *y = &x;
+int main(){
+	int x, a, b, c;
+	int *y = &x;
+	a = 4;
+	b = a * a;
+	c = a << 4;
 	*y = 2;
-   
+	for(int i = -3; i < 4; i++){
+		printf("%d\n", y[i]);
+	}
+	return 0;
 }
