35843bcf2b31133be52e2ac6142745720d661f7e
Ryan Joseph Sims <ryanjsims@cs.arizona.edu>
2017-09-15 13:28:12 -0700

    proj03 files

---
+++ proj03/reverseWords.c
@@ -0,0 +1,28 @@
+/* reverseWords.c
+ *
+ * Author: TODO
+ *         (skeleton by Russ Lewis)
+ *
+ * Reverses the words of output.  Has some (intentional) buffer-overflow
+ * bugs.
+ */
+
+#include <stdio.h>
+
+
+int main()
+{
+	char buf1[32];
+	char buf2[32];
+	char buf3[32];
+
+
+	/* initialize the strings in all three buffers */
+	strcpy(buf1, "BUF1");
+	strcpy(buf2, "BUF2");
+	strcpy(buf3, "BUF3");
+
+
+	TODO: put your code here
+}
+
