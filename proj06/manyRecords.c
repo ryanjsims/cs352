bd7375de8b59ecd6f00e9b644dfd50e7fb107492
Ryan Sims <ryanjsims@email.arizona.edu>
2017-10-17 15:16:14 -0700

    Added comments

--- proj06/manyRecords.c
+++ proj06/manyRecords.c
@@ -1,3 +1,12 @@
+/*
+ * File: manyRecords.c
+ * Author: Ryan Sims
+ * Purpose: manyRecords stores records in structs using a dynamically allocated
+ * array, expanding it as necessary. After, it prints out various indices in the
+ * array as requested by the user.
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,9 +16,32 @@ typedef struct Record{
 	char word[16];
 } Record;
 
+
+/* 
+ * printRecord(Record *rec) -- printRecord takes a pointer to a single Record
+ * and prints the Record to stdout.
+ */
 void printRecord(Record *rec);
+
+/*
+ * fillRecord(Record *buf, int index) -- fillRecord takes a pointer to an array of Records
+ * and fills the Record at index with info from stdin. Checks for formatting errors and
+ * returns -1 on error, or the number of items read otherwise.
+ */
 int fillRecord(Record *buf, int index);
+
+/*
+ * checkForEnd() -- checkForEnd reads a single string from stdin and
+ * returns 1 if it matches "END", 0 otherwise.
+ */
 int checkForEnd();
+
+/*
+ * extendBuffer(Record **buffer, int *size) -- extendBuffer takes a pointer to a buffer of Records
+ * and a pointer to an int containing the size of the buffer and doubles the size of the buffer.
+ * Modifies both buffer and size.
+ */
+void extendBuffer(Record **buffer, int *size);
 
 int main(int argc, char **argv){
 	int size = 1;
@@ -17,26 +49,15 @@ int main(int argc, char **argv){
 	int rc = 0, i = 0;
 	do{
 		if(i == size){
-			printf("Need to extend the array; the buffer is full (with %d records)\n", size);
-			printf("  Before extending the array, element 0 is: ");
-			printRecord(buffer);
-			size = 2 * size;
-			Record *new = malloc(size * sizeof(Record));
-			memcpy(new, buffer, (size / 2) * sizeof(Record));
-			free(buffer);
-			buffer = new;
-			printf("  After  extending the array, element 0 is: ");
-			printRecord(buffer);
+			extendBuffer(&buffer, &size);
 		}
 		rc = fillRecord(buffer, i);
 		if(rc > 0)
 			i++;
 		if(rc == 0 && !checkForEnd()){
-			//Format error
 			free(buffer);
 			return 1;
 		} else if(rc < 0){
-			//Format Error
 			free(buffer);
 			return 1;
 		}
@@ -63,10 +84,37 @@ int main(int argc, char **argv){
 
 }
 
+/*
+ * printRecord(Record *rec) -- printRecord takes a pointer to a single Record
+ * and prints the Record to stdout.
+ */
 void printRecord(Record *rec){
 	 printf("%d %d %s\n", rec->firstInt, rec->secondInt, rec->word);
 }
 
+/*
+ * extendBuffer(Record **buffer, int *size) -- extendBuffer takes a pointer to a buffer of Records
+ * and a pointer to an int containing the size of the buffer and doubles the size of the buffer.
+ * Modifies both buffer and size.
+ */
+void extendBuffer(Record **buffer, int *size) {
+	printf("Need to extend the array; the buffer is full (with %d records)\n", *size);
+	printf("  Before extending the array, element 0 is: ");
+	printRecord(*buffer);
+	*size = 2 * *size;
+	Record *new = malloc(*size * sizeof(Record));
+	memcpy(new, *buffer, (*size / 2) * sizeof(Record));
+	free(*buffer);
+	*buffer = new;
+	printf("  After  extending the array, element 0 is: ");
+	printRecord(*buffer);
+}
+
+/*
+ * fillRecord(Record *buf, int index) -- fillRecord takes a pointer to an array of Records
+ * and fills the Record at index with info from stdin. Checks for formatting errors and
+ * returns -1 on error, or the number of items read otherwise.
+ */
 int fillRecord(Record *buf, int index){
 	int rc = scanf("%d %d %15s", &buf[index].firstInt, &buf[index].secondInt, buf[index].word);
 	if(rc != 3 && rc != 0){
@@ -76,6 +124,10 @@ int fillRecord(Record *buf, int index){
 	return rc;
 }
 
+/*
+ * checkForEnd() -- checkForEnd reads a single string from stdin and
+ * returns 1 if it matches "END", 0 otherwise.
+ */
 int checkForEnd(){
 	printf("scanf() returned 0, so now we'll check to see if the string is \"END\"...\n");
 	char temp[4];
